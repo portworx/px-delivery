16e69e6983490bf9ab054cad19c9427de2cf367d
eshanks <eshanks@purestorage.com>
2023-01-27 16:22:46 -0600

    Standardizing Order Handlers

--- lib/order.go
+++ lib/order.go
@@ -178,111 +178,11 @@ func PxbbqOrderHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(session.Values["authenticated"])
 	fmt.Println(session.Values["email"])
 	fmt.Println(r.Method)
+
 	//generate Order ID
 	orderNum := generateOrderID()
 
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		fmt.Println("Not Authenticated")
-	} else {
-		fmt.Println("Authenticated")
-	}
-
-	if r.Method == "GET" {
-		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-			fmt.Println("Order form requested, but unauthenticated; redirecting to login page.")
-			t, _ := template.ParseFiles("./static/login.html")
-			t.Execute(w, nil)
-		} else {
-			fmt.Printf("should allow order")
-			t, _ := template.ParseFiles("./static/pxbbq_order.html")
-			t.Execute(w, nil)
-		}
-	} else {
-		r.ParseForm()
-		statusData := PageData{
-			PageTitle: "Order Status",
-			Message:   fmt.Sprintf("Your order has been received. Order number %v", orderNum),
-		}
-
-		//write to mongo
-		fmt.Printf("Order submitted by: ")
-		fmt.Println(session.Values["email"].(string))
-		email := session.Values["email"].(string)
-		main := r.FormValue("main")
-		side1 := r.FormValue("side1")
-		side2 := r.FormValue("side2")
-		drink := r.FormValue("drink")
-
-		registerOrder(orderNum, email, main, side1, side2, drink)
-
-		//Display Operation Status Page to User
-		orderStatus(w, r, statusData)
-	}
-}
-
-func McdOrderHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method, "on URL:", r.URL)
-	session, _ := store.Get(r, "cookie-name")
-	fmt.Println(session.Values["authenticated"])
-	fmt.Println(session.Values["email"])
-	fmt.Println(r.Method)
-	//generate Order ID
-	orderNum := generateOrderID()
-
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		fmt.Println("Not Authenticated")
-	} else {
-		fmt.Println("Authenticated")
-	}
-
-	if r.Method == "GET" {
-		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-			fmt.Println("Order form requested, but unauthenticated; redirecting to login page.")
-			t, _ := template.ParseFiles("./static/login.html")
-			t.Execute(w, nil)
-		} else {
-			fmt.Printf("should allow order")
-			t, _ := template.ParseFiles("./static/mcd_order.html")
-			t.Execute(w, nil)
-		}
-	} else {
-		r.ParseForm()
-		statusData := PageData{
-			PageTitle: "Order Status",
-			Message:   fmt.Sprintf("Your order has been received. Order number %v", orderNum),
-		}
-
-		//write to mongo
-		fmt.Printf("Order submitted by: ")
-		fmt.Println(session.Values["email"].(string))
-		email := session.Values["email"].(string)
-		main := r.FormValue("main")
-		side1 := r.FormValue("side1")
-		side2 := r.FormValue("side2")
-		drink := r.FormValue("drink")
-
-		registerOrder(orderNum, email, main, side1, side2, drink)
-
-		//Display Operation Status Page to User
-		orderStatus(w, r, statusData)
-	}
-}
-
-func CentralperkOrderHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method, "on URL:", r.URL)
-	session, _ := store.Get(r, "cookie-name")
-	fmt.Println(session.Values["authenticated"])
-	fmt.Println(session.Values["email"])
-	fmt.Println(r.Method)
-	//generate Order ID
-	orderNum := generateOrderID()
-
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		fmt.Println("Not Authenticated")
-	} else {
-		fmt.Println("Authenticated")
-	}
-
+	//verify the user is authenticated and if not send them to the login page instead
 	if r.Method == "GET" {
 		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 			fmt.Println("Order form requested, but unauthenticated; redirecting to login page.")
@@ -300,23 +200,162 @@ func CentralperkOrderHandler(w http.ResponseWriter, r *http.Request) {
 			Message:   fmt.Sprintf("Your order has been received. Order number %v", orderNum),
 		}
 
-		//write to kafka
-		fmt.Printf("Order submitted by: ")
-		fmt.Println(session.Values["email"].(string))
+		//Organize form submission data for a write to storage
+		currentTime := time.Now() //used in the order submission
 		email := session.Values["email"].(string)
 		main := r.FormValue("main")
 		side1 := r.FormValue("side1")
 		side2 := r.FormValue("side2")
 		drink := r.FormValue("drink")
 
-		//get address of user
+		//retrieve address from customer's account
 		myAddress := GetAddress(email)
 		street1 := myAddress.Street1
 		street2 := myAddress.Street2
 		city := myAddress.City
 		state := myAddress.State
 		zipcode := myAddress.Zipcode
+		orderDate := currentTime.Format("2 January 2006")
 
+		//log order to std out - Can be used for troubleshooting
+		fmt.Printf("Order submitted by: ")
+		fmt.Println(session.Values["email"].(string))
+		fmt.Println("Order Taken by Portworx BBQ")
+		fmt.Println("Ordered : " + main)
+		fmt.Println("Ordered : " + side1)
+		fmt.Println("Ordered : " + side2)
+		fmt.Println("Ordered : " + drink)
+		fmt.Println("Street1 : " + street1)
+		fmt.Println("Street2 : " + street2)
+		fmt.Println("City  : " + city)
+		fmt.Println("State  : " + state)
+		fmt.Println("Zip  : " + zipcode)
+		fmt.Print("########")
+
+		//submit order to storage
+		SubmitOrder(orderNum, orderDate, email, "PXBBQ", main, side1, side2, drink, street1, street2, city, state, zipcode)
+
+		//Display Operation Status Page to User
+		orderStatus(w, r, statusData)
+	}
+}
+
+func McdOrderHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method, "on URL:", r.URL)
+	session, _ := store.Get(r, "cookie-name")
+	fmt.Println(session.Values["authenticated"])
+	fmt.Println(session.Values["email"])
+	fmt.Println(r.Method)
+
+	//generate Order ID
+	orderNum := generateOrderID()
+
+	//verify the user is authenticated and if not send them to the login page instead
+	if r.Method == "GET" {
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			fmt.Println("Order form requested, but unauthenticated; redirecting to login page.")
+			t, _ := template.ParseFiles("./static/login.html")
+			t.Execute(w, nil)
+		} else {
+			fmt.Printf("should allow order")
+			t, _ := template.ParseFiles("./static/mcd_order.html")
+			t.Execute(w, nil)
+		}
+	} else {
+		r.ParseForm()
+		statusData := PageData{
+			PageTitle: "Order Status",
+			Message:   fmt.Sprintf("Your order has been received. Order number %v", orderNum),
+		}
+
+		//Organize form submission data for a write to storage
+		currentTime := time.Now() //used in the order submission
+		email := session.Values["email"].(string)
+		main := r.FormValue("main")
+		side1 := r.FormValue("side1")
+		side2 := r.FormValue("side2")
+		drink := r.FormValue("drink")
+
+		//retrieve address from customer's account
+		myAddress := GetAddress(email)
+		street1 := myAddress.Street1
+		street2 := myAddress.Street2
+		city := myAddress.City
+		state := myAddress.State
+		zipcode := myAddress.Zipcode
+		orderDate := currentTime.Format("2 January 2006")
+
+		//log order to std out - Can be used for troubleshooting
+		fmt.Printf("Order submitted by: ")
+		fmt.Println(session.Values["email"].(string))
+		fmt.Println("Order Taken by McDowells")
+		fmt.Println("Ordered : " + main)
+		fmt.Println("Ordered : " + side1)
+		fmt.Println("Ordered : " + side2)
+		fmt.Println("Ordered : " + drink)
+		fmt.Println("Street1 : " + street1)
+		fmt.Println("Street2 : " + street2)
+		fmt.Println("City  : " + city)
+		fmt.Println("State  : " + state)
+		fmt.Println("Zip  : " + zipcode)
+		fmt.Print("########")
+
+		//submit order to storage
+		SubmitOrder(orderNum, orderDate, email, "McDowells", main, side1, side2, drink, street1, street2, city, state, zipcode)
+
+		//Display Operation Status Page to User
+		orderStatus(w, r, statusData)
+	}
+}
+
+func CentralperkOrderHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method, "on URL:", r.URL)
+	session, _ := store.Get(r, "cookie-name")
+	fmt.Println(session.Values["authenticated"])
+	fmt.Println(session.Values["email"])
+	fmt.Println(r.Method)
+
+	//generate Order ID
+	orderNum := generateOrderID()
+
+	//verify the user is authenticated and if not send them to the login page instead
+	if r.Method == "GET" {
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			fmt.Println("Order form requested, but unauthenticated; redirecting to login page.")
+			t, _ := template.ParseFiles("./static/login.html")
+			t.Execute(w, nil)
+		} else {
+			fmt.Printf("should allow order")
+			t, _ := template.ParseFiles("./static/centralperk_order.html")
+			t.Execute(w, nil)
+		}
+	} else {
+		r.ParseForm()
+		statusData := PageData{
+			PageTitle: "Order Status",
+			Message:   fmt.Sprintf("Your order has been received. Order number %v", orderNum),
+		}
+
+		//Organize form submission data for a write to storage
+		currentTime := time.Now() //used in the order submission
+		email := session.Values["email"].(string)
+		main := r.FormValue("main")
+		side1 := r.FormValue("side1")
+		side2 := r.FormValue("side2")
+		drink := r.FormValue("drink")
+
+		//retrieve address from customer's account
+		myAddress := GetAddress(email)
+		street1 := myAddress.Street1
+		street2 := myAddress.Street2
+		city := myAddress.City
+		state := myAddress.State
+		zipcode := myAddress.Zipcode
+		orderDate := currentTime.Format("2 January 2006")
+
+		//log order to std out - Can be used for troubleshooting
+		fmt.Printf("Order submitted by: ")
+		fmt.Println(session.Values["email"].(string))
 		fmt.Println("Order Taken by Central Perk")
 		fmt.Println("Ordered : " + main)
 		fmt.Println("Ordered : " + side1)
@@ -327,38 +366,36 @@ func CentralperkOrderHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("City  : " + city)
 		fmt.Println("State  : " + state)
 		fmt.Println("Zip  : " + zipcode)
-
 		fmt.Print("########")
-		currentTime := time.Now()
-		orderDate := currentTime.Format("2 January 2006")
 
-		//submit order to Kafka
-		SubmitOrder(orderNum, orderDate, email, "Central Perk", main, side1, side2, drink)
+		//submit order to storage
+		SubmitOrder(orderNum, orderDate, email, "Central Perk", main, side1, side2, drink, street1, street2, city, state, zipcode)
 
 		//Display Operation Status Page to User
 		orderStatus(w, r, statusData)
 	}
 }
 
-func SubmitOrder(orderNum int, orderDate string, email string, restaurant string, main string, side1 string, side2 string, drink string) {
+func SubmitOrder(orderNum int, orderDate string, email string, restaurant string, main string, side1 string, side2 string, drink string, street1 string, street2 string, city string, state string, zipcode string) {
 
-	fmt.Println("I'm begging the Submit Order Function Now - Trying to Submit to Kafka")
-	fmt.Println("#########")
-	fmt.Println("email is : " + email)
-	fmt.Println("restaurant is : " + restaurant)
-	fmt.Println("main is : " + main)
-	fmt.Println("side1 is : " + side1)
-	fmt.Println("side2 is : " + side2)
-	fmt.Println("drink is : " + drink)
-	fmt.Println("#########")
+	//FOR TROUBLESHOOTING INPUT VALIDATION
+	//fmt.Println("I'm begging the Submit Order Function Now - Trying to Submit to Kafka")
+	//fmt.Println("#########")
+	//fmt.Println("email is : " + email)
+	//fmt.Println("restaurant is : " + restaurant)
+	//fmt.Println("main is : " + main)
+	//fmt.Println("side1 is : " + side1)
+	//fmt.Println("side2 is : " + side2)
+	//fmt.Println("drink is : " + drink)
+	//fmt.Println("#########")
+
+	//create a kafka producer - connection variables come from environment variables KAFKA_HOST and KAFKA_PORT
 	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaHost + ":" + kafkaPort})
-
 	if err != nil {
 		fmt.Printf("Failed to create producer: %s\n", err)
 	}
 
-	fmt.Printf("Created Producer %v\n", p)
-
+	//configure a kafka delivery channel
 	deliveryChan := make(chan kafka.Event)
 
 	// Produce messages to topic (asynchronously)
@@ -372,21 +409,22 @@ func SubmitOrder(orderNum int, orderDate string, email string, restaurant string
 		Side2:       side2,
 		Drink:       drink,
 		Date:        orderDate,
-		Street1:     "123 main street",
-		Street2:     "",
-		City:        "springfield",
-		State:       "IL",
-		Zip:         "60606",
+		Street1:     street1,
+		Street2:     street2,
+		City:        city,
+		State:       state,
+		Zip:         zipcode,
 		OrderStatus: "Pending",
 	}
 
-	//testing
+	//convert msg to a json object and store it in payload
 	payload, err := json.Marshal(msg)
 
+	//push msg to the kafka queue
 	err = p.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
 		Value:          payload,
-		Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+		Headers:        []kafka.Header{{Key: "PX-Delivery", Value: []byte("header values are binary")}},
 	}, deliveryChan)
 	if err != nil {
 		fmt.Printf("Produce failed: %v\n", err)
